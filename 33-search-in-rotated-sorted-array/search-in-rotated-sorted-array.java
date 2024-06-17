dc88db81b8650212884b636803c73c00fd6aa9e5
Abhinav Kumar Jha <abhinavkrjha10@gmail.com>
2024-06-17 12:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (96.18%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    int findElement(int[] ar ,int start,int end,int target){
+        if(start<=end){
+            int mid = (start+end)/2;
+            if(ar[mid]==target){
+                return mid;
+            }
+            else if(ar[mid]>target)
+            return findElement(ar, start, mid-1, target);
+            else
+             return findElement(ar, mid+1, end, target);
+        }
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+        int pivot = 0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1])
+            pivot=i;
+            else
+            break;
+        }
+        int l1 = findElement(nums,0,pivot,target);
+        int l2 = findElement(nums,pivot+1,nums.length-1,target);
+        if(l1!=-1)
+        return l1;
+        if(l2!=-1)
+        return l2;
+        return -1;
+        
+    }
+}
