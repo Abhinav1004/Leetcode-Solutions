1514934a549401ab08ad66b19a6dd9dcc8f176a2
Abhinav Kumar Jha <abhinavkrjha10@gmail.com>
2025-01-18 07:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (28.06%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-       int i = nums.size()-1;
-        while(i>0 && (nums[i]<=nums[i-1]))
-        i--;
-        if(i>0){
-            int pivot = i-1;int j;
-            for(j=nums.size()-1;j>i;j--){
-                if(nums[j]>nums[pivot]){
-                    break;
-                }
+    void reverse_array(vector<int>& ar,int start,int end){
+        int i;int j;
+        for(i=start,j=end;i<j;i++,j--){
+            swap(ar[i],ar[j]);
+        }
+    }
+    int find_smallest(vector<int> ar,int start,int end){
+        int pivot_value = ar[start-1];
+        int smallest_number = INT_MAX;
+        int smallest_pos =  start;
+        for(int i=end;i>=start;i--){
+            if(ar[i]>pivot_value && ar[i]<smallest_number){
+                smallest_number=ar[i];
+                smallest_pos = i;
             }
-            swap(nums[j],nums[pivot]);
-            reverse(nums.begin()+i,nums.end());
         }
-        else{
-            sort(nums.begin(),nums.end());
+        return smallest_pos;
+    }
+    void nextPermutation(vector<int>& nums) {
+     int pivot = -1;int i;int n = nums.size();
+    for(i=n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            pivot=i;
+            break;
         }
+    }
+    if(pivot==-1){
+        reverse_array(nums,0,n-1);
+    }
+    else
+        {
+            int ele_pos = find_smallest(nums,pivot+1,n-1);
+            swap(nums[pivot],nums[ele_pos]);
+            reverse_array(nums,pivot+1,n-1);
+            // reverse(nums,nums.begin()+pivot+1,nums.end());
+        }   
     }
 };
